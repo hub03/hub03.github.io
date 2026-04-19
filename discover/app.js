16fc514d3f83bbf440a07847c04b7cefd1544760
hub03 <trebmal14@gmail.com>
2026-04-19 19:43:39 +0200

    Create app.js

---
+++ discover/app.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const form = document.getElementById("discoverForm");
+
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const data = {
+        nbPersonnes: nbPersonnes.value,
+        logement: logement.value,
+        superficie: superficie.value,
+        teletravail: teletravail.value,
+        devices: devices.value,
+        tv: tv.value,
+        aboTv: aboTv.value,
+        alarme: alarme.value,
+        fai: fai.value,
+        prixBox: prixBox.value,
+        engagementBox: engagementBox.value,
+        tech: tech.value,
+        operateur: operateur.value,
+        prixMobile: prixMobile.value,
+        engagementMobile: engagementMobile.value,
+        data: data.value,
+        lignes: lignes.value
+      };
+
+      let fiches = JSON.parse(localStorage.getItem("discover")) || [];
+      fiches.push(data);
+
+      localStorage.setItem("discover", JSON.stringify(fiches));
+
+      alert("Fiche enregistrée !");
+      window.location.href = "index.html";
+    });
+  }
+
+});
